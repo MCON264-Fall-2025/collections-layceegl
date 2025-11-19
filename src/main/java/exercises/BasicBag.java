54390bbdfb7a4380bfddaf03d9a71827e6c93911
layceegl <lgluck2@student.touro.edu>
2025-11-19 14:26:23 -0500

    Update BasicBag.java

--- src/main/java/exercises/BasicBag.java
+++ src/main/java/exercises/BasicBag.java
@@ -6,22 +6,48 @@ import edu.touro.mcon264.apps.collections.BagInterface;
 public class BasicBag<T> extends ArrayCollection<T> implements BagInterface<T> {
     @Override
     public T grab() {
-        return null;
+        if (size() == 0)
+            return null;
+        int randomIndex = (int) (Math.random() * size());
+        T element = get(randomIndex);
+        remove(element);   // Remove by value using inherited remove(T)
+        return element;
     }
 
     @Override
     public int count(T target) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < size(); i++) {
+            if (get(i).equals(target))
+                count++;
+        }
+        return count;
     }
 
     @Override
     public int removeAll(T target) {
-        return 0;
+        int removedCount = 0;
+        // Keep removing target until it no longer exists
+        while (remove(target)) {
+            removedCount++;
+        }
+        return removedCount;
     }
 
     @Override
     public void clear() {
+        // Remove elements until bag is empty
+        while (!isEmpty()) {
+            remove(elements[numElements - 1]);
+        }
+    }
 
+
+    protected T get(int index) {
+        if (index < 0 || index >= numElements) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return elements[index];
     }
 
     // TODO: implement the methods in the BagInterface using an array as the underlying data structure
